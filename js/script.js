ec163768f28b724028b993f7a8c0a07acd1e4fe9
lokollotje <55492758+lokollotje@users.noreply.github.com>
2020-01-28 18:38:29 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@
 /*eslint-env browser*/
 /*eslint 'no-console': 0*/
 
+function veranderImage() {
+    document.getElementsByClassName('favorited').src = "favorited.svg";
+}
+
+document.getElementsByClassName('star').addEventListener('click', veranderImage);
